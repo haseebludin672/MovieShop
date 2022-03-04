c104f343aadaecc00a175eaaf20abaa55c51d35f
19254 <19254@DESKTOP-FSS9L23>
2022-03-04 15:54:06 -0500

    Add project files.

---
+++ ApplicationCore/Contracts/Services/IMovieService.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Contracts.Services
+{
+    public interface IMovieService
+    {
+        //have all the business logic methods relating to movies
+        List<MovieCardModel> GetTop30GrossingMovies();
+        
+    }
+}
---
+++ MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Models;
+using System.Diagnostics;
+
+namespace MovieShopMVC.Controllers
+{
+    public class HomeController : Controller
+
+
+    {[HttpGet]
+        public IActionResult Index()
+        {
+            var movieService = new MovieService();
+            //model data passing it to the view
+            var movies = movieService.GetTop30GrossingMovies();
+            //our controllers are very thin/lean
+            //most of your logic should come from other dependencies , services
+            //Interfaces
+            //void method (int x, ImovieService service);
+            //method (20, movieservice);
+            return View(movies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
